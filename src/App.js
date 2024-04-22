06186867636de36825254b83cdb8b5330784dfea
name <god@god.com>
2024-04-22 12:33:04 +0900

    두번째

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Home, About, NotFound, Movie } from './pages';
+import Menu from './Menu'
+import Sidebar from './Sidebar'
+import Button from './Button'
+
+
+class App extends Component {
+  homeMenu = [
+    {
+      url: "/",
+      name: "HOME"
+    },
+    {
+      url: "/about",
+      name: "ABOUT"
+    },
+    {
+      url: "/movies",
+      name: "MOVIE"
+    },
+  ]
+  state = {
+    open: false,
+    movies:[] 
+  }
+  showSidebar = () => {
+    this.setState({ open: !this.state.open })
+  }
+
+  //api사용
+  componentDidMount(){ 
+    fetch('https://yts.mx/api/v2/list_movies.json?limit=5')
+    .then( res => res.json())
+    .then( result => { 
+      const {data: {movies}} = result 
+      this.setState({movies}) 
+    }) 
+  }
+
+  render(){
+    const { open,movies } = this.state
+    const { homeMenu } = this
+    return (
+      <div className="App">
+        <Button handleClick={this.showSidebar}>Menu</Button>
+        <Sidebar open={open}>
+          <Menu menus={homeMenu}></Menu>
+        </Sidebar>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route path="/movies" element={<Movie movies={movies}/>}>
+            <Route path=":movieId" element={<Movie/>} />
+          </Route>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/Movie.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useParams, NavLink, useSearchParams, useLocation } from "react-router-dom";
+/* import posts from '../postData' */
+import './Movie.css'
+
+function Movie({movies}){
+    const params = useParams();      //params:url 파라미터를 담고있는 객체
+   
+    let [searchParams, setSearchParams] = useSearchParams()
+    const applyActiveColor = ({ isActive }) => (isActive ? {color: 'orangered'} : {}) //클릭했을때 색상변경
+
+    const changeQueryString = (e) => {
+        const filter = e.target.value
+        if(filter){
+            setSearchParams({ filter }) //?filter=사용자가 검색한
+        }else{
+            setSearchParams({})    //url 쿼리스트링 초기화
+        }
+    }
+    const QueryNavLink = ({ to, children, ...props }) => {
+        const location = useLocation();
+        console.log(location)
+        return <NavLink to={to + location.search} {...props}>{children}</NavLink>
+      }
+      //필터링된 목록에서 특정목록
+      const moviesFiltered = movies
+    .filter( movie => {
+        const filter = searchParams.get('filter')
+        if(!filter) return true;
+        const title = movie.title.toLowerCase()
+        return title.includes(filter.toLowerCase())
+    })
+    const movie = moviesFiltered[params.movieId] // 필터링된 목록에서 특정 포스트 글 선택하기
+    return (
+        <>
+            {/* 쿼리스트링을 이용한 검색 */}
+            <br/><input className="filter-movie" value={searchParams.get('filter') || ""} onChange={changeQueryString} placeholder="검색할 영화를 ..."/>
+
+            {/* 특정 블로그 포스트 */}
+            {movie ? 
+                <div className="movie-container">
+                    <h1>{movie.title_long}</h1>
+                    {/* 이미지 추가 */}
+                    <img src={movie.medium_cover_image}  alt={movie.title}></img>
+                    <p>{movie.synopsis}</p>
+                    <span>{movie.genres.join(",")}</span>
+                </div>   :
+                <h1>MOVIE PAGE</h1>}
+
+            {/* 블로그 포스트 전체목록  */}
+            {moviesFiltered
+            .map( (movie, id) => {
+                return (
+                    <QueryNavLink key={id} to={`/movies/${id}`} className="movie-item" style={applyActiveColor}>{movie.title}</QueryNavLink>
+                )
+            })}
+        </>
+    )
+}
+export default Movie
